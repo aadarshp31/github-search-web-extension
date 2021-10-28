45d50aa1b5ed5dbb97e30f618a9afdcb86df36e0
adarsh.pandey <adarsh.pandey@zohocorp.com>
2021-10-28 15:44:05 +0530

    added commands to search github users by username

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,14 +1,59 @@
+import axios from 'axios';
 import * as vscode from 'vscode';
+import User from './entities/User';
 
-export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "search-blog-extension" is now active in the web extension host!');
+export async function activate(context: vscode.ExtensionContext) {
+	let disposable = [];
 
-	let disposable = vscode.commands.registerCommand('search-blog-extension.searchWdsBlogExample', () => {
-		vscode.window.showInformationMessage('Hello World from Search Blog Extension in a web extension host!');
+	// Utils
+	const getGithubUserByUsername = async (username: string = ""): Promise<any> => {
+
+		if(username === ""){
+			throw new Error("Please enter a valid github username to continue");
+		}
+
+		const res = await axios.get(`https://api.github.com/users/${username}`);
+		return res.data;
+	} 
+	
+	const getTopGithubUsers = vscode.commands.registerCommand('search-github-users-extension.getTopGithubUsers', async () => {
+
+		
+		// Load top github users by id
+		const res = await axios.get("https://api.github.com/users");
+
+		const users: any[] = res.data.map((user: User) => ({
+			label: user.login,
+			detail: user.html_url,
+			link: user.html_url
+		}))
+
+		const selectedUser: any = await vscode.window.showQuickPick(users, {placeHolder: "Select any user to open in browser"});
+		vscode.env.openExternal(selectedUser?.link);
 	});
 
-	context.subscriptions.push(disposable);
+	const searchGithubUser = vscode.commands.registerCommand('search-github-users-extension.searchGithubUser', async () => {
+		try {
+			const userEnteredUsername = await vscode.window.showInputBox({placeHolder: "Enter a github username to search", ignoreFocusOut: true, title: "Github Username", value: ""});
+			const user = await getGithubUserByUsername(userEnteredUsername);
+	
+			const userList: any[] = [{
+				label: user.name || user.login,
+				detail: user.bio || user.html_url,
+				link: user.html_url
+			}];		
+	
+			const selectedUser = await vscode.window.showQuickPick(userList, {placeHolder: "Select any user to open in browser"});
+			vscode.env.openExternal(selectedUser?.link);
+		} catch (error: any) {
+			vscode.window.showErrorMessage(error.message);
+		}
+	})
+
+	disposable.push(getTopGithubUsers, searchGithubUser);
+
+	context.subscriptions.push(...disposable);
 }
 
 export function deactivate() {}
